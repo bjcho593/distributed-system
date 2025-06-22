5f2e61dac8f40df098ccba94d44dd5312cbf2e59
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-22 11:46:10 -0500

    Delete login-service directory

--- login-service/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-var app = builder.Build();
-
-app.MapGet("/login/status", () => Results.Ok("Login Service is running ✅"));
-app.MapControllers();
-
-app.Run();
